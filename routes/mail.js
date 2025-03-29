7b59576e1885f84fbd03ef7879418b56d8ea31d7
PAO <4A61C003@stust.edu.tw>
2025-03-29 10:23:11 +0800

    重設密碼方法修正

--- routes/mail.js
+++ routes/mail.js
@@ -102,7 +102,7 @@ router.get("/verify-token", async (req, res) => {
 });
 
 // 重置密碼
-router.post("/reset-password", async (req, res) => {
+router.put("/reset-password", async (req, res) => {
   const output = {
     success: false,
     result: null,
